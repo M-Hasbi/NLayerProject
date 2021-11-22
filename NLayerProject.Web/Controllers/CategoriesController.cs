65db247a3ebb071674f80dba1afe48fc3618ab7a
M-Hasbi <kaynakhasbi1@gmail.com>
2021-11-22 18:51:12 +0100

    create : CategoriesController.cs

---
+++ NLayerProject.Web/Controllers/CategoriesController.cs
@@ -0,0 +1,23 @@
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Mvc;
+using NLayerProject.Core.Services;
+
+namespace NLayerProject.Web.Controllers
+{
+    public class CategoriesController : Controller
+    {
+        private readonly ICategoryService _categoryService;
+        private readonly IMapper _mapper;
+
+        public CategoriesController(ICategoryService categoryService,Mapper mapper)
+        {
+            _categoryService = categoryService;
+            _mapper = mapper;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
